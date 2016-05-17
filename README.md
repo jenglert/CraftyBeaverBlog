a3a765dc571a43bc1ee9ccbf501bb2de9c9e037d
Jim Englert <englert.james@gmail.com>
2016-05-16 21:32:50 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# CraftyBeaverBlog #
+
+## Build & Run ##
+
+```sh
+$ cd CraftyBeaverBlog
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/cbeav/app/BlogServlet.scala
@@ -0,0 +1,16 @@
+package cbeav.app
+
+import org.scalatra._
+
+class BlogServlet extends CraftybeaverblogStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
